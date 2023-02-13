cad92e56464817168edcd74ee1078ab5aed5c8f5
Justin Papreck <justin.papreck@gmail.com>
2023-02-13 11:33:45 -0800

    Final Commits to the quarterly queries

--- Animal_Shelter_Top_Quarterly_Adoptions.sql
+++ Animal_Shelter_Top_Quarterly_Adoptions.sql
@@ -134,3 +134,93 @@ GROUP BY    GROUPING SETS
             DATE_PART('quarter', adoption_date)
 
 ORDER BY    species ASC, year ASC, quarter ASC;
+
+------------------------------------------------------------------------------------
+-- Starting with quarters, since the quarter function will cross years ending the last quarter on Jan 15
+WITH adoption_quarters
+AS
+(
+SELECT 	Species,
+		MAKE_DATE (	CAST (DATE_PART ('year', adoption_date) AS INT),
+					CASE 
+						WHEN DATE_PART ('month', adoption_date) < 4
+							THEN 1
+						WHEN DATE_PART ('month', adoption_date) BETWEEN 4 AND 6
+							THEN 4
+						WHEN DATE_PART ('month', adoption_date) BETWEEN 7 AND 9
+							THEN 7
+						WHEN DATE_PART ('month', adoption_date) > 9
+							THEN 10
+					END,
+					1
+				 ) AS quarter_start
+FROM 	adoptions
+)
+-- SELECT * FROM adoption_quarters ORDER BY species, quarter_start;
+,quarterly_adoptions
+AS
+(
+SELECT 	COALESCE (species, 'All species') AS species,
+		quarter_start,
+		COUNT (*) AS quarterly_adoptions,
+		COUNT (*) - COALESCE (
+					-- For quarters with no previous adoptions use 0, not NULL 
+							 	FIRST_VALUE (COUNT (*))
+							 	OVER    (PARTITION BY species
+							 		  ORDER BY quarter_start ASC
+								   	  RANGE BETWEEN 	INTERVAL '3 months' PRECEDING 
+												AND 
+												INTERVAL '3 months' PRECEDING
+						 		        )
+							, 0)
+		AS adoption_difference_from_previous_quarter,
+		CASE 	
+			WHEN	quarter_start =	FIRST_VALUE (quarter_start) 
+									OVER (PARTITION BY species
+										  ORDER BY quarter_start ASC
+										  RANGE BETWEEN 	UNBOUNDED PRECEDING
+															AND
+															UNBOUNDED FOLLOWING
+										 )
+			THEN 	0
+			ELSE 	NULL
+		END 	AS zero_for_first_quarter
+FROM 	adoption_quarters
+GROUP BY	GROUPING SETS 	((quarter_start, species), 
+							 (quarter_start)
+							)
+)
+-- SELECT * FROM quarterly_adoptions ORDER BY species, quarter_start;
+,quarterly_adoptions_with_rank
+AS
+(
+SELECT 	*,
+		RANK ()
+		OVER (	PARTITION BY species
+				ORDER BY 	COALESCE (zero_for_first_quarter, adoption_difference_from_previous_quarter) DESC,
+							-- First quarters are 0, all others NULL
+							quarter_start DESC)
+		AS quarter_rank
+FROM 	quarterly_adoptions
+)
+-- SELECT * FROM quarterly_adoptions_with_rank ORDER BY species, quarter_rank, quarter_start;
+SELECT 	species,
+		CAST (DATE_PART ('year', quarter_start) AS INT) AS year,
+		CAST (DATE_PART ('quarter', quarter_start) AS INT) AS quarter,
+		adoption_difference_from_previous_quarter,
+		quarterly_adoptions
+FROM 	quarterly_adoptions_with_rank
+WHERE 	quarter_rank <= 5
+ORDER BY 	species ASC,
+			adoption_difference_from_previous_quarter DESC,
+			quarter_start ASC;
+
+
+
+
+SELECT * FROM adoption_quarters ORDER BY species, quarter_start
+
+
+
+SELECT LAG(admission_date) OVER (ORDER BY admission_date ASC) = LEAD(admission_date) OVER (ORDER BY admission_date DESC)
+FROM animals
